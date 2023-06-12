18210fd078c732741d90724915780fec50ba8bae
wangb <wangbinyq@gmail.com>
2023-06-12 13:03:44 +0800

    export init sync

--- mod.ts
+++ mod.ts
@@ -1,2 +1,6 @@
 // @deno-types="./mod.d.ts"
-export { Vibrato, default as initVibrato } from "./pkg/vibrato_deno.js";
+export {
+  Vibrato,
+  default as initVibrato,
+  initSync as initVibratoSync,
+} from "./pkg/vibrato_deno.js";
